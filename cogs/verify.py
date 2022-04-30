89b3ea3286baff4b74c17ea86aee3fc54d4c2bd0
Varchasva Khare <72777353+varchasvkhare@users.noreply.github.com>
2022-04-30 15:46:29 +0530

    e

--- cogs/verify.py
+++ cogs/verify.py
@@ -13,7 +13,7 @@ class Verify(commands.Cog):
         """Server Verification"""
         class ViewWithButton(discord.ui.View):
             @discord.ui.button(style=discord.ButtonStyle.green, label='Verify')
-            async def click_me_button(self, button: discord.ui.Button, interaction: discord.Interaction):
+            async def click_me_button(self, interaction: discord.Interaction, button: discord.ui.Button):
                 verified=discord.utils.get(ctx.guild.roles, name="[0] Verified")
                 await interaction.response.send_message('I have give you access to the server!', ephemeral=True)
                 await interaction.user.add_roles(verified)
