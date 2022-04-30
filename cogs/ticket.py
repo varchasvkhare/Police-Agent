d57b2c59e4a3a7ac10fd131f71a69e0e4eaaa085
Varchasva Khare <72777353+varchasvkhare@users.noreply.github.com>
2022-04-30 19:46:07 +0530

    d

--- cogs/ticket.py
+++ cogs/ticket.py
@@ -16,16 +16,19 @@ class Ticket(commands.Cog):
         class Ticket(discord.ui.View):
             @discord.ui.button(style=discord.ButtonStyle.green, label='Create a Ticket', custom_id='ticket')
             async def ticket_create_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+                category = discord.utils.get(ctx.guild.categories, name='⪻ᚔᚓᚒᚑ᚜office᚛ᚑᚒᚓᚔ⪼')
                 mod=discord.utils.get(ctx.guild.roles, name="Moderation Team")
+                ticket_name=discord.utils.get(ctx.guild.channels, f"Ticket-{interaction.user.id}")
                 await interaction.guild.create_text_channel(
-                    name=f"Ticket {interaction.user.id}",
+                    category=category,
+                    name=f"Ticket-{interaction.user.id}",
                     overwrites={
                         interaction.guild.default_role: discord.PermissionOverwrite(view_channel=False),
                         interaction.user: discord.PermissionOverwrite(view_channel=True),
-                        mod: discord.PermissionOverwrite(view_channel=True),
+                        mod: discord.PermissionOverwrite(view_channel=True)
                     }
                 )
-                await interaction.response.send_message(f'{name}', ephemeral=True)
+                await interaction.response.send_message(f'{ticket_name}', ephemeral=True)
         
         embed = discord.Embed(
             title=f'Create a Ticket',
