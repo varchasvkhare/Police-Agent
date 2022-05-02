97461120584d52cbc693215b3c2b1c93c7023624
Varchasva Khare <72777353+varchasvkhare@users.noreply.github.com>
2022-05-02 23:17:52 +0530

    q

--- cogs/amari.py
+++ cogs/amari.py
@@ -17,14 +17,14 @@ class Amari(commands.Cog):
     async def amari(self, ctx: commands.Context, membed: discord.Member) -> None:
         """Check your amari level"""
         amari = AmariClient("e5e71cc6daa28c3d9bc848ee.435561.0813a19d4b9746eacf0d4e68857")
-        lb = await amari.fetch_leaderboard(760134264242700320, weekly=True, limit=3)
-        top_users_ids = [data[1] for data in list(lb.users.items())]
+        id = ctx.author.id
+        user = await amari.fetch_user(760134264242700320, id)
         
         embed = discord.Embed(
             title=f"Top Users",
             description = inspect.cleandoc(
                 f"""
-                {top_users_ids}
+                {user.level}
                 """
             ),
             color=0x9C84EF
