fa3626216714a852d8934471b67e32c775b7cc74
Varchasva Khare <72777353+varchasvkhare@users.noreply.github.com>
2022-05-02 23:31:54 +0530

    as

--- cogs/amari.py
+++ cogs/amari.py
@@ -14,23 +14,45 @@ class Amari(commands.Cog):
     @commands.command(
         name="amari", aliases = ["level"]
     )
-    async def amari(self, ctx: commands.Context, membed: discord.Member) -> None:
+    async def amari(self, ctx: commands.Context, member: discord.Member = None) -> None:
         """Check your amari level"""
         amari = AmariClient("e5e71cc6daa28c3d9bc848ee.435561.0813a19d4b9746eacf0d4e68857")
-        id = ctx.author.id
-        user = await amari.fetch_user(760134264242700320, id)
         
-        embed = discord.Embed(
-            title=f"Top Users",
-            description = inspect.cleandoc(
-                f"""
-                {user.level}
-                """
-            ),
-            color=0x9C84EF
-        )
+        if member == None:
+            
+            ctx_id = ctx.author.id
+            ctx_user = await amari.fetch_user(760134264242700320, ctx_id)
         
-        await ctx.send(embed=embed)
+            embed = discord.Embed(
+                title=f"{ctx.author.name}'s amari status",
+                description = inspect.cleandoc(
+                    f"""
+                    Level - {ctx_user.level}
+                    XP - {ctx_user.exp} {ctx_user.position}
+                    Weekly XP - {ctx_user.weeklyexp}
+                    """
+                ),
+                color=0x9C84EF
+            )
+            await ctx.send(embed=embed)
+            
+        else:
+            
+            member_id = member.id
+            member_user = await amari.fetch_user(760134264242700320, member_id)
+            
+            embed = discord.Embed(
+                title=f"{ctx.author.name}'s amari status",
+                description = inspect.cleandoc(
+                    f"""
+                    Level - {member_user.level}
+                    XP - {member_user.exp} {ctx_user.position}
+                    Weekly XP - {member_user.weeklyexp}
+                    """
+                ),
+                color=0x9C84EF
+            )
+            await ctx.send(embed=embed)
         
 async def setup(bot):
     await bot.add_cog(Amari(bot))
