c75d739cf4fcdc672fca110b400ac288759b7411
Varchasva Khare <72777353+varchasvkhare@users.noreply.github.com>
2022-05-03 16:38:41 +0530

    q

--- cogs/selfroles.py
+++ cogs/selfroles.py
@@ -16,55 +16,95 @@ class SelfRoles(commands.Cog):
     async def selfroles(self, ctx: commands.Context) -> None:
         """ping roles"""
         class Announcements(discord.ui.View):
-            @discord.ui.button(style=discord.ButtonStyle.blurple, label='Announcements', custom_id='announcements', emoji='<:announcements:970937379839967332>')
-            async def announcement_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-                announcement=discord.utils.get(ctx.guild.roles, name="• ❯ Announcements")
-                if announcement in interaction.user.roles:
-                    await interaction.response.send_message(f'I have removed {announcement.mention} from you', ephemeral=True)
-                    await interaction.user.remove_roles(announcement)
+            @discord.ui.button(
+                style = discord.ButtonStyle.green,
+                label = 'Giveaways Access',
+                custom_id = 'gaw_access',
+                emoji = '<:gaw:971001484261064726>'
+            )
+            async def gaw_access_button(
+                self,
+                interaction: discord.Interaction,
+                button: discord.ui.button
+            ):
+                gaw_access = discord.utils.get(ctx.guild.roles, name="Giveaway Access")
+                gaw_bl = discord.utils.get(ctx.guild.roles, name = 'Giveaway Blacklist')
+                
+                if gaw_bl in interaction.user.roles:
+                    await interaction.response.send_message('Hold on bro! You are blacklisted from giveaways so i cant hand over this role to you.', ephemeral=True)
+                elif gaw_access in interaction.user.roles:
+                    await interaction.response.send_message(f'I have removed {gaw_access.mention} from you', ephemeral=True)
+                    await interaction.user.remove_roles(gaw_access)
                 else:
-                    await interaction.response.send_message(f'I have added {announcement.mention} to you', ephemeral=True)
-                    await interaction.user.add_roles(announcement)
+                    await interaction.response.send_message(f'I have added {gaw_access.mention} to you', ephemeral=True)
+                    await interaction.user.add_roles(gaw_access)
+                      
+            @discord.ui.button(
+                style=discord.ButtonStyle.green,
+                label='Event Access',
+                custom_id='event_access',
+                emoji='<:event:971001579593428992>'
+            )
+            async def event_access_button(
+                self,
+                interaction: discord.Interaction,
+                button: discord.ui.Button
+            ):
+                event_access = discord.utils.get(ctx.guild.roles, name="Event Access")
+                event_bl = discord.utils.get(ctx.guild.roles, name = 'Event Blacklist')
+                
+                if event_bl in interaction.user.roles:
+                    await interaction.response.send_message('Hold on bro! You are blacklisted from events so i cant hand over this role to you.', ephemeral=True)
+                elif event_access in interaction.user.roles:
+                    await interaction.response.send_message(f'I have removed {event_access.mention} from you', ephemeral=True)
+                    await interaction.user.remove_roles(event_access)
+                else:
+                    await interaction.response.send_message(f'I have added {event_access.mention} to you', ephemeral=True)
+                    await interaction.user.add_roles(event_access)
+            
+            @discord.ui.button(
+                style=discord.ButtonStyle.green,
+                label='Bot Channel Access',
+                custom_id='bot_access',
+                emoji='<:bota:971001648786837564>'
+            )
+            async def bot_access_button(
+                self,
+                interaction: discord.Interaction,
+                button: discord.ui.Button
+            ):
+                bot_access = discord.utils.get(ctx.guild.roles, name="Event Access")
+                
+                if bot_access in interaction.user.roles:
+                    await interaction.response.send_message(f'I have removed {bot_access.mention} from you', ephemeral=True)
+                    await interaction.user.remove_roles(bot_access)
+                else:
+                    await interaction.response.send_message(f'I have added {bot_access.mention} to you', ephemeral=True)
+                    await interaction.user.add_roles(bot_access)
         
-            @discord.ui.button(style=discord.ButtonStyle.blurple, label='Giveaways', custom_id='giveaways', emoji='<a:giveaways:970959495708676146>')
-            async def gaw_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-                gaws=discord.utils.get(ctx.guild.roles, name="• ❯ Giveaways")
-                if gaws in interaction.user.roles:
-                    await interaction.response.send_message(f'I have removed {gaws.mention} from you', ephemeral=True)
-                    await interaction.user.remove_roles(gaws)
-                else:
-                    await interaction.response.send_message(f'I have added {gaws.mention} to you', ephemeral=True)
-                    await interaction.user.add_roles(gaws)
-                    
-            @discord.ui.button(style=discord.ButtonStyle.blurple, label='Events', custom_id='events', emoji='<a:giveaways:970959495708676146>')
-            async def gaw_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-                event=discord.utils.get(ctx.guild.roles, name="• ❯ Events")
-                if event in interaction.user.roles:
-                    await interaction.response.send_message(f'I have removed {event.mention} from you', ephemeral=True)
-                    await interaction.user.remove_roles(event)
-                else:
-                    await interaction.response.send_message(f'I have added {event.mention} to you', ephemeral=True)
-                    await interaction.user.add_roles(event)
-                    
-            @discord.ui.button(style=discord.ButtonStyle.blurple, label='Status', custom_id='status', emoji='<a:giveaways:970959495708676146>')
-            async def gaw_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-                sta=discord.utils.get(ctx.guild.roles, name="• ❯ Status")
-                if sta in interaction.user.roles:
-                    await interaction.response.send_message(f'I have removed {sta.mention} from you', ephemeral=True)
-                    await interaction.user.remove_roles(sta)
-                else:
-                    await interaction.response.send_message(f'I have added {sta.mention} to you', ephemeral=True)
-                    await interaction.user.add_roles(sta)
+
                 
                 
         embed = discord.Embed(
-            title=f'Ping roles below dummy',
+            #title=f'Ping roles below dummy',
             description=inspect.cleandoc(
                 f"""
+                **Access Roles -**
                 
+                ・Giveaway Access - Access to giveaway channels
+                ・Event Access - Access to event channels
+                ・Bot Access - Access to all bot channels
+                
+                **Ping Roles -**
+                
+                ・Announcements - Get notified on any server updates, staff applications etc
+                ・Status - Get notified on bot's uptime and downtime
+                ・Partnership - Get pinged whenever we partner with a server
+                ・Giveaways - Get pinged on any ongoing giveaway
+                ・Events - Get notified on any ongoing event
                 """
             ),
-            color=0x111111
+            color=0xfcf55f
         )
         await ctx.send(embed=embed, view=Announcements())
         
