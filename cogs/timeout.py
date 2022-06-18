6677a5b95a6d74b0fea0b39d6362edfbe2b4ba14
Varchasva Khare <varchasvkhare@gmail.com>
2022-06-18 14:47:03 +0530

    Fixed Time

--- cogs/timeout.py
+++ cogs/timeout.py
@@ -16,7 +16,7 @@ class Timeout(commands.Cog):
     @commands.has_permissions(moderate_members=True)
     async def timeout(self, ctx: commands.Context, member: discord.Member, time=None, *, reason=None) -> None:
         """Timeout a user"""
-        #log_channel = discord.utils.get(ctx.guild.channels, id=970211808830967879)
+        ##log_channel = discord.utils.get(ctx.guild.channels, id=970211808830967879)
         time_convert = {"s":1, "m":60, "h":3600,"d":86400}
         duration= int(time[0]) * time_convert[time[-1]]
         
